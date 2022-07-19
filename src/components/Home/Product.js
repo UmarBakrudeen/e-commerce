44b290456b287ad54ca57bbcae68597eb99713e0
UmarBakrudeen <thalaivaumar9501s.com>
2022-07-19 12:00:53 +0530

    Commit

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -6,6 +6,9 @@ import { useParams } from "react-router";
 import Skeleton from "react-loading-skeleton";
 import Header from "../../shared/Header/Header";
 
+// import sofa from "../../assets/images/sofa.jpg";
+// import sofa2 from "../../assets/images/sofa2.jpg";
+
 const Product = () => {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
@@ -29,10 +32,10 @@ const Product = () => {
   const Loading = () => {
     return (
       <>
-        <div className="col-md-6">
+        <div className="pp__items">
           <Skeleton height={400} />
         </div>
-        <div className="col-md-6">
+        <div className="text__container">
           <Skeleton height={50} width={300} style={{ lineHeight: 2 }} />
           <Skeleton height={75} />
           <Skeleton height={25} width={150} />
@@ -48,7 +51,7 @@ const Product = () => {
   const ShowProduct = () => {
     return (
       <>
-        <div className="col-md-6">
+        {/* <div className="col-md-6">
           <img
             src={product.image}
             alt={product.title}
@@ -74,6 +77,106 @@ const Product = () => {
           <Link to="/cart" className="btn btn-dark ms-2 px-3 py-2">
             Go to Cart
           </Link>
+        </div> */}
+
+        {/* <div className="product-container">
+          <div className="p-container">
+            <div className="p-image">
+              <img
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                className="pro-img"
+              />
+            </div>
+            <div className="child-image">
+              <div className="c-image1">
+                <img
+                  src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                  alt="product-img"
+                />
+              </div>
+              <div className="c-image2">
+                <img
+                  src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                  alt="product-img"
+                />
+              </div>
+              <div className="c-image3">
+                <img
+                  src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                  alt="product-img"
+                />
+              </div>
+              <div className="c-image3">
+                <img
+                  src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                  alt="product-img"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="product-content">
+            <h3>llana</h3>
+            <p>
+              Complete complaints require the physical or electronic signature
+              of the owner.
+            </p>
+            <div className="price-details">
+              <strong>&#36;</strong>
+              <strong>430.99</strong>
+            </div>
+            <div className="review-container">
+              <div className="review">
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+                <i class="fa fa-star"></i>
+              </div>
+              <div className="rating">
+                <a href="#">441 Reviews</a>
+              </div>
+            </div>
+
+            <div className="btn-holder">
+              <button className="btn-buy">Buy Now</button>
+              <button className="btn-add">Add to basket</button>
+            </div>
+          </div>
+        </div> */}
+
+        <div className="product__page__container">
+          <div className="pp__content">
+            <div className="pp__lists">
+              <div className="pp__items">
+                <img src={product.image} alt="" className="pro__img" />
+                <div className="image__content">
+                  <img src={product.image} alt="" />
+                  <img src={product.image} alt="" />
+                  <img src={product.image} alt="" />
+                  <img src={product.image} alt="" />
+                </div>
+              </div>
+
+              <div className="text__container">
+                <div className="text__content">
+                  <h1> {product.title} </h1>
+                  <div className="desc">{product.description}</div>
+                  <div className=" rating ">
+                    Rating {product.rating && product.rating.rate}
+                    <i className="bx bxs-star"> </i>
+                  </div>
+                  <div className=" price "> ${product.price}</div>
+
+                  <div className="btn__holder">
+                    <button className="btn-buy">Buy Now</button>
+                    <a href="/" className="btn-buy">
+                      Add to basket
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
@@ -82,8 +185,13 @@ const Product = () => {
   return (
     <Fragment>
       <Header />
-      <div className="container py-5">
+      {/* <div className="container py-5">
         <div className="row py-5">
+          {loading ? <Loading /> : <ShowProduct />}
+        </div>
+      </div> */}
+      <div className="main__product">
+        <div className="main__product__content">
           {loading ? <Loading /> : <ShowProduct />}
         </div>
       </div>
